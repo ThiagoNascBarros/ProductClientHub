344331a8c24670d032fb79df9dff4b0016e29da5
ThiagoNascBarros <thiago171nasciment@gmail.com>
2025-12-30 22:51:34 -0300

    feat: Creating client Controller
    Signed-off-by: ThiagoNascBarros <thiago171nasciment@gmail.com>

---
+++ src/ProductClientHub.API/Controller/ClientsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductClientHub.API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Register()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("{id}", Name = "Id of Client")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete()
+        {
+            return Ok();
+        }
+    }
+}
