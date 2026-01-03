b478a1967f3e877593c9dcceb2685510f57007a0
ThiagoNascBarros <thiago171nasciment@gmail.com>
2026-01-03 16:02:42 -0300

    feat: Create response of client
    Signed-off-by: ThiagoNascBarros <thiago171nasciment@gmail.com>

--- src/ProductClientHub.Communication/Requests/RequestClientJson.cs
+++ src/ProductClientHub.Communication/Requests/RequestClientJson.cs
@@ -1,8 +1,23 @@
-﻿namespace ProductClientHub.Communication.Requests
+﻿using ProductClientHub.Exceptions.Constructor;
+
+namespace ProductClientHub.Communication.Requests
 {
     public class RequestClientJson
     {
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
+
+        public RequestClientJson(string name, string email)
+        {
+            try
+            {
+                Name = name.Trim();
+                Email = email.Trim();
+            }
+            catch (ErrorOnTrimException)
+            {
+                throw new ErrorOnTrimException();
+            }
+        }
     }
 }
---
+++ src/ProductClientHub.Exception/Constructor/ErrorOnTrimException.cs
@@ -0,0 +1,13 @@
+﻿namespace ProductClientHub.Exceptions.Constructor
+{
+    public class ErrorOnTrimException : Exception
+    {
+        public ErrorOnTrimException()
+        {
+        }
+
+        public ErrorOnTrimException(string? message) : base(message)
+        {
+        }
+    }
+}
